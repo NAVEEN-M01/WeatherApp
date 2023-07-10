251d068a7db8a867a66f5310eb9bd8d3c17bdec4
Naveen M <126571407+NAVEEN-M01@users.noreply.github.com>
2023-07-10 18:04:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import axios from 'axios';
+function App() {
+  return (
+    <div className="App">
+   const url='https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid='
+   
+
+    </div>
+  );
+}
+
+export default App;
